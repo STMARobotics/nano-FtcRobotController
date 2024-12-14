bd32654cfdc33aa742a072e8d49a77c7c7282b98
molliemur <mollie.muraski@stmarobotics.org>
2024-12-14 13:27:36 -0600

    changed angle more

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainOpMode.java
@@ -118,7 +118,7 @@ public class MainOpMode extends LinearOpMode {
             } else if (gamepad2.y){
                 wrist.moveToPosition(.2);
             } else if (gamepad2.x){
-               wrist.moveToPosition(.3);
+               wrist.moveToPosition(.25);
            }
 
             if (gamepad2.dpad_left){
